c070bd15a18303b484a785e62d95a4245fbfa300
prajwal <kondubhatlav.eprajwal@gmail.com>
2020-02-27 00:33:45 -0800

    Added Fragments and Info option on the Action Bar

--- main/java/com/prajwal/mysjsu/StudentDetailsActivity.java
+++ main/java/com/prajwal/mysjsu/StudentDetailsActivity.java
@@ -2,8 +2,14 @@ package com.prajwal.mysjsu;
 
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -11,11 +17,45 @@ public class StudentDetailsActivity extends AppCompatActivity {
 
     TextView studentID, name, email, phone, address;
     FloatingActionButton fab;
+    Fragment infoFragment;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.info_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.infoItem: {
+                FragmentManager manager = getSupportFragmentManager();
+                infoFragment = manager.findFragmentById(R.id.fragment);
+                FragmentTransaction ft = manager.beginTransaction();
+//                ft.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out ).hide(infoFragment).commit();
+                if(infoFragment.isHidden()){
+                    ft.show(infoFragment);
+                } else {
+                    ft.hide(infoFragment);
+                }
+                ft.commit();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_details);
+
+        FragmentManager manager = getSupportFragmentManager();
+        infoFragment = manager.findFragmentById(R.id.fragment);
+        FragmentTransaction ft = manager.beginTransaction();
+        ft.hide(infoFragment);
+        ft.commit();
+
 
         studentID = findViewById(R.id.studentIdVal);
         name = findViewById(R.id.nameValue);
